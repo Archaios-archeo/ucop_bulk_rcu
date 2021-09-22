e6f0704db2472b4ddade8940febe12ee96b548e3
Julie Gravier <juliecatherine.gravier@gmail.com>
2021-09-22 11:28:39 +0200

    update significance B5 and 6

--- add_bulk_1_to_6_significance.R
+++ add_bulk_1_to_6_significance.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(readxl)
 library(openxlsx)
 
-data_2019_200_1 <- read_excel(path = "data/ucop_data_2019_2020_1_v4.xlsx")
+data_2019_200_1 <- read_excel(path = "data/ucop_data_2019_2020_1_v5.xlsx")
 
 # Bulk number 1
 bulk_1_part_1 <- read_excel(path = "sorties/finales/500_features_bulk_1/UCOP_heritage_features_bulk_1_part_1_update.xlsx",
@@ -111,6 +111,45 @@ bulk_4 %>%
 write.xlsx(x = bulk_4, file = "sorties/finales/500_features_bulk_4/significance_bulk_4.xlsx")
 
 
+# Bulk number 5
+bulk_5 <- read_excel(path = "sorties/finales/500_features_bulk_5/UCOP_heritage_features_bulk_number_5.xlsx",
+                     sheet = "NameGroup")
+bulk_5 <- bulk_5 %>%
+  bind_cols(read_excel(path = "sorties/finales/500_features_bulk_5/UCOP_heritage_features_bulk_number_5.xlsx",
+                       sheet = "DescriptionGroup"))
 
+bulk_5 %>%
+  mutate(OS_Number = str_sub(string = NAME.E41, start = 1, end = 8)) %>%
+  left_join(x = ., y = data_2019_200_1 %>%
+              select(OS_Number, `Feature significance RCU`),
+            by = "OS_Number") %>%
+  mutate(GENERAL_DESCRIPTION_TYPE.E55 = paste0(GENERAL_DESCRIPTION_TYPE.E55, "|", "Summary of Significance")) %>%
+  mutate(GENERAL_DESCRIPTION.E62 = if_else(
+    condition = !is.na(`Feature significance RCU`), true = paste0(GENERAL_DESCRIPTION.E62, "|", `Feature significance RCU`),
+    paste0(GENERAL_DESCRIPTION.E62, "|", "x")
+  )) %>%
+  select(NAME.E41, GENERAL_DESCRIPTION_TYPE.E55, GENERAL_DESCRIPTION.E62) -> bulk_5
 
+write.xlsx(x = bulk_5, file = "sorties/finales/500_features_bulk_5/significance_bulk_5.xlsx")
+
+# Bulk number 6
+bulk_6 <- read_excel(path = "sorties/finales/500_features_bulk_6/UCOP_heritage_features_bulk_number_6.xlsx",
+                     sheet = "NameGroup")
+bulk_6 <- bulk_6 %>%
+  bind_cols(read_excel(path = "sorties/finales/500_features_bulk_6/UCOP_heritage_features_bulk_number_6.xlsx",
+                       sheet = "DescriptionGroup"))
+
+bulk_6 %>%
+  mutate(OS_Number = str_sub(string = NAME.E41, start = 1, end = 8)) %>%
+  left_join(x = ., y = data_2019_200_1 %>%
+              select(OS_Number, `Feature significance RCU`),
+            by = "OS_Number") %>%
+  mutate(GENERAL_DESCRIPTION_TYPE.E55 = paste0(GENERAL_DESCRIPTION_TYPE.E55, "|", "Summary of Significance")) %>%
+  mutate(GENERAL_DESCRIPTION.E62 = if_else(
+    condition = !is.na(`Feature significance RCU`), true = paste0(GENERAL_DESCRIPTION.E62, "|", `Feature significance RCU`),
+    paste0(GENERAL_DESCRIPTION.E62, "|", "x")
+  )) %>%
+  select(NAME.E41, GENERAL_DESCRIPTION_TYPE.E55, GENERAL_DESCRIPTION.E62) -> bulk_6
+
+write.xlsx(x = bulk_6, file = "sorties/finales/500_features_bulk_6/significance_bulk_6.xlsx")
 
