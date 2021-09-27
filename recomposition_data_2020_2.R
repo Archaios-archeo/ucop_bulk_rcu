5b88b05a4936674b57ff94c21ebe960d0e06b587
Julie Gravier <juliecatherine.gravier@gmail.com>
2021-09-27 18:11:18 +0200

    recompo str_to_title, etc.

--- recomposition_data_2020_2.R
+++ recomposition_data_2020_2.R
@@ -29,3 +29,54 @@ list_of_dataset <- list("DATA" = data_2020_2_revu)
 write.xlsx(list_of_dataset, 
            file = "data/BDD_OS_2020_2_v3.xlsx", 
            append = TRUE)
+
+
+#### des éléments qui doivent être revus ####
+data_2020_2 <- read_excel(path = "data/BDD_OS_2020_2_v3.xlsx")
+
+data_2020_2 %>% select(`Main periods`) %>% unique()
+
+data_2020_2 %>%
+  mutate(`Description date` = as.character(`Description date`)) %>%
+  mutate(`Site accessibility` = str_to_title(`Site accessibility`)) %>%
+  mutate(`Main periods` = case_when(
+    `Main periods` == "Late Ottoman|Modern" ~ "Islamic|Modern",
+    `Main periods`== "Late Ottoman|Kingdom of Saudi Arabia" ~ "Islamic|Modern",
+    `Main periods`== "Late Ottoman" ~ "Islamic",
+    `Main periods`== "unknown" ~ "Unknown",
+    `Main periods` == "Late Ottoman|Contemporary|Late Ottoman|Modern|WWI|Kingdom of Saudi Arabia|WWII|Modern" ~ "Islamic|Modern",
+    `Main periods` == "Late Ottoman|Modern|WWI|Kingdom of Saudi Arabia|WWII|Modern" ~ "Islamic|Modern",
+    `Main periods`== "Contemporary|Late Ottoman|Modern|WWI|Kingdom of Saudi Arabia|WWII|Modern" ~ "Islamic|Modern",
+    TRUE ~ `Main periods`
+  )) %>%
+  mutate(Periods = case_when(
+    `Main periods` == "Islamic|Modern" ~ "Late Ottoman|Unknown",
+    `Main periods` == "Islamic" ~ "Late Ottoman",
+    `Main periods` == "Modern" ~ "Unknown",
+    `Main periods` == "Unknown" ~ "Unknown",
+    `Main periods` == "unknown|Dadanite" ~ "Unknown|Dadanite",
+    `Main periods` == "Nabataean Kingdom" ~ "Nabataean Kingdom",
+    `Main periods` == "Dadanite" ~ "Dadanite"
+  )) %>%
+  mutate(`Main periods` = case_when(
+    `Main periods` == "unknown|Dadanite" ~ "Unknown|Iron Age/Pre-Classical",
+    `Main periods` == "Dadanite" ~ "Iron Age/Pre-Classical",
+    `Main periods` == "Pre-Islamic" ~ "Classical/Pre-Islamic",
+    `Main periods` == "Nabataean Kingdom" ~ "Classical/Pre-Islamic",
+    TRUE ~ `Main periods`
+  )) %>% 
+  mutate(Periods = if_else(is.na(Periods), "Unknown", Periods)) %>%
+  mutate(`Threat Levels` = str_to_title(`Threat Levels`)) %>%
+  mutate(Description = case_when(
+    !is.na(`Description TH`) ~ `Description TH`,
+    is.na(`Description TH`) & !is.na(`Description Corrigée`) ~ `Description Corrigée`,
+    TRUE ~ Description
+  )) %>%
+  mutate(Description = if_else(is.na(Description), "x", Description)) -> data_2020_2
+
+
+
+
+
+
+
